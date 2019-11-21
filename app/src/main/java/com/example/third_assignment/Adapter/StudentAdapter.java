57e6a1a594f29818d735f32970e8a1f61ebc7ad5
bibektamang873 <bibektamang873@gmail.com>
2019-11-21 11:41:41 +0545

    student adapter

--- app/src/main/java/com/example/third_assignment/Adapter/StudentAdapter.java
+++ app/src/main/java/com/example/third_assignment/Adapter/StudentAdapter.java
@@ -1,12 +1,66 @@
 package com.example.third_assignment.Adapter;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class StudentAdapter  {
+import com.example.third_assignment.Model.Student;
+import com.example.third_assignment.R;
+
+import java.util.List;
+
+public class StudentAdapter extends RecyclerView.Adapter<StudentAdapter.MyHolder> {
+
+    List<Student> students;
+
+    public StudentAdapter(List<Student> students) {
+        this.students = students;
+    }
+
+    @NonNull
+    @Override
+    public MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.student_design,parent,false);
+        MyHolder myHolder = new MyHolder(view);
+        return myHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyHolder holder, int position) {
+        final Student student = students.get(position);
+        holder.fullname.setText(student.getFullName());
+        holder.age.setText(student.getAge());
+        holder.gender.setText(student.getGender());
+        holder.address.setText(student.getAddress());
+        holder.studentImage.set
+        holder.btnDelete.setOnClickListener(
+    }
+
+    @Override
+    public int getItemCount() {
+        return students.size();
+    }
 
     public class MyHolder extends RecyclerView.ViewHolder{
-        S
+        ImageView studentImage;
+        TextView fullname, age, address, gender;
+        Button btnDelete;
 
+        public MyHolder(@NonNull View itemView) {
+            super(itemView);
+            studentImage = itemView.findViewById(R.id.imageView);
+            fullname = itemView.findViewById(R.id.tvName);
+            age = itemView.findViewById(R.id.tvAge);
+            address = itemView.findViewById(R.id.tvAddress);
+            gender = itemView.findViewById(R.id.tvGender);
+            btnDelete = itemView.findViewById(R.id.btnDelete);
+        }
     }
 }
 
